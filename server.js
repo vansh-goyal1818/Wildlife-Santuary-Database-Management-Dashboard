c5b860890f21517ac3a9212eacd795edd487f3aa
vansh-goyal1818 <goyal.vansh0118@gmail.com>
2026-05-26 16:16:20 +0530

    Refactor server.js for improved structure and clarity

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("__dirname"));
 
 /* TABLES */
 app.get("/tables", (req, res) => {
